24fe03c08648833001e3fc7c7a11c5a092de3044
18702Idea <18702Idea@qq.com>
2020-04-25 14:44:17 +0800

    merge hello.java

--- Hello/src/main/java/com/test/Hello.java
+++ Hello/src/main/java/com/test/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("hello idea");
+        System.out.println("eclipse");
+        System.out.println("idea");
     }
 
 }
